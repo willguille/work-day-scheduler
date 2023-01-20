c8df346effa824a5d7bc8b2d83351d578fb67367
wilguille <willyguille97@gmail.com>
2023-01-20 20:02:45 +0000

    Added timeblocks to div container for each bussiness hour, with a save button at the end of each one

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
 // Display the current day at the top of the calendar
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+// Present timeblocks for standard business hours
+for (var i = 9; i < 18; i++) {
+    var timeblock = $("<div>", { class: "timeblock row" });
+    var timeblockHour = $("<div>", { class: "col-1" }).text(`${i}:00`);
+    var task = $("<textarea>", { class: "task col-10" });
+    var saveBtn = $("<button>", { class: "fas fa-save col-1" });
+    timeblock.append(timeblockHour, task, saveBtn);
+    $(".container").append(timeblock);
+}
+
